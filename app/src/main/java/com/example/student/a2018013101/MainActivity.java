4e65e62d87b577399b8272dea560bb56b2aaabd3
miowoo <miowoo821@gmail.com>
2018-02-09 13:36:06 +0800

    照、存、讀(剛剛的會塞爆記憶體，這版寫法比較好)

--- app/src/main/java/com/example/student/a2018013101/MainActivity.java
+++ app/src/main/java/com/example/student/a2018013101/MainActivity.java
@@ -33,13 +33,14 @@ public class MainActivity extends AppCompatActivity {
         //接下來新增傳回參數時要幹嘛(寫新方法)
     }
     public void click2(View v){//照相存起來再讀出來到img
+
         Intent it=new Intent(MediaStore.ACTION_IMAGE_CAPTURE);//呼叫相機唷
 
         File f = new File(getExternalFilesDir("PHOTO"), "myphoto.jpg");//給一個路徑喔
         it.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(f));//把照片放到這個路徑喔
         startActivityForResult(it,66);//要抓東西回來的intent要用這個(參數1：INTENT，參數2；識別碼)
         //接下來新增傳回參數時要幹嘛(寫新方法)
-
+        Log.d("GGGGGGGGGGGGGGGGGGGG","YUKJTRHWEFD:"+f);
 
 
     }
